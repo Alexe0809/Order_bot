2920ff40b22f10cfe9d08d3261d694d1c987a0b5
Alex <apodolako44@gmail.com>
2026-08-18 20:02:01 +0300

    first commit1

---
+++ app/bot.py
@@ -0,0 +1,19 @@
+from config import BOT_TOKEN
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_handler(message:Message):
+    await message.answer("Hello! How can I help you")
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
